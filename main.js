ca5614318f6a8f0a2cf46ebff29ef24102ac0fca
Sebastian08LR <156454864+Sebastian08LR@users.noreply.github.com>
2024-07-29 22:03:06 -0500

    docs: :memo: adds the structure for the documentation
    there were added commets that will have the steps of how to use for each module and its options

---
+++ main.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+// Uso de cada uno de los apartados de los modulos
+
+// MODULO DE PELICULAS
+/* 
+    1. 
+
+
+
+
+
+*/
+
+// MODULO DE USUARIOS
+/* 
+    1. 
+
+
+
+
+
+*/
+
+// MODULO DE BOLETAS
+/* 
+    1. 
+
+
+
+
+
+*/
+
+// MODULO DE PAGOS
+/* 
+    1. 
+
+
+
+
+
+*/
