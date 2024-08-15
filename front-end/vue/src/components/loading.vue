626e527111338e3dff86ba71a852889f60722994
Sebastian08LR <156454864+Sebastian08LR@users.noreply.github.com>
2024-08-15 00:01:15 -0500

    feat: :sparkles: add a  new component to show when the data is loading

---
+++ front-end/vue/src/components/loading.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="spinner"></div>
+</template>
+
+<script>    
+
+</script>
+
+<style scoped>
+.spinner {  
+  justify-self: center;
+  align-self: center;
+  border: 4px solid rgba(255, 255, 255, 0.3);
+  border-top: 4px solid #fff;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+}
+</style>
